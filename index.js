32f57395f28d17a1821442be1f0a54228ed2a63e
Alejandro Cantero <alejandro@loonfy.com>
2020-06-19 10:49:58 +0200

    remove deprecated rule

--- index.js
+++ index.js
@@ -281,12 +281,6 @@ module.exports = {
 		"@typescript-eslint/explicit-function-return-type": "off",
 		"@typescript-eslint/no-inferrable-types": "off",
 		"@typescript-eslint/no-use-before-define": "off",
-		"@typescript-eslint/camelcase": [
-			"error",
-			{
-				properties: "never"
-			}
-		],
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/explicit-member-accessibility": [
 			'error',
